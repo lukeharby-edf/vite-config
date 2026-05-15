117092b2c748f578f56c23da9107c4c36e4dbb01
Harby <Luke.Harby@edfenergy.com>
2026-05-15 10:52:22 +0100

    Update everything (and env stuff)

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,54 @@
-import { defineConfig } from 'vite';
-import sassGlobImports from 'vite-plugin-sass-glob-import';
-import htmlMinifier from 'vite-plugin-html-minifier'
+import { defineConfig, loadEnv } from "vite";
+import path from "node:path";
+import sassGlobImports from "vite-plugin-sass-glob-import";
+import htmlMinifier from "vite-plugin-html-minifier";
+
+let entrypoints = null;
 
 const entry = {
-    // js
-    main: './src/js/app',
-    // section css
-    'editor': './src/scss/editor.scss'
+  // js
+  main: "./src/js/app",
+  // section css
+  editor: "./src/scss/editor.scss",
 };
 
-export default defineConfig({
+const components = {
+  accordion: "components/accordion.html",
+  calendar: "components/calendar.html",
+};
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const isComponents = env.VITE_IS_COMPONENTS;
+  isComponents
+    ? (entrypoints = { ...entry, ...components })
+    : (entrypoints = entry);
+  console.log(`isComponents: ${isComponents}`);
+
+  return {
     plugins: [
-        sassGlobImports(),
-        htmlMinifier({
-            minify: true,
-        }),
+      sassGlobImports(),
+      htmlMinifier({
+        minify: true,
+      }),
     ],
     build: {
-        rollupOptions: {
-            input: {
-                ...entry,
-                accordion: 'components/accordion.html',
-                calendar: 'components/calendar.html'
-            },
-            output: {
-                assetFileNames: () => {
-                    return `[name][extname]`;
-                },
-                chunkFileNames: 'main.min.js',
-                entryFileNames: 'main.min.js',
-            },
+      rollupOptions: {
+        input: {
+          ...entrypoints,
         },
-        target: 'es2015',
-        outDir: 'app/build/',
-        sourcemap: true
+        output: {
+          assetFileNames: ({ names }) => {
+            const ext = path.extname(names.find(() => true));
+            return `[name][extname]`;
+          },
+          chunkFileNames: "main.min.js",
+          entryFileNames: "main.min.js",
+        },
+      },
+      target: "es2015",
+      outDir: "app/build/",
+      sourcemap: true,
     },
+  };
 });
