3c0fd2e88b5ce545bfaa1001b8ecedec132a22d8
Luke Harby <lukeharby@Lukes-MacBook-Pro.local>
2026-05-26 19:57:43 +0100

    Add subpages

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,5 @@
 import '../scss/app.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const url = `https://edfenergytest.learning.peoplefluent.net/ekp/api/learningPath?format=json`;
-    const username = import.meta.env.VITE_USERNAME;
-    const password = import.meta.env.VITE_PASSWORD;
-
-    const headers = new Headers();
-    headers.set('Authorization', 'Basic ' + btoa(username + ':' + password));
-
-    fetch(url, {
-        headers,
-        mode: 'no-cors',
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.log('Something went wrong.', err);
-    });
-
+    console.log('App is ready');
 });
